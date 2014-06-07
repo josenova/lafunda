8a6baf9e1302d72bb9bc7ed5f334fb486f7bc944
josenova <novajose@gmail.com>
2014-06-07 16:12:31 -0400

    ignore database yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'lafunda'
+set :repo_url, 'git@bitbucket.org:kinbar/lafunda.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/lafundac/rails_apps/lafunda'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -20,7 +20,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
@@ -56,3 +56,53 @@ namespace :deploy do
   end
 
 end
+
+
+
+=begin
+require 'bundler/capistrano'
+
+set :application, "Your App's Name"
+set :repository,  "git@github.com:you-github-username/your-app-repository.git"
+set :deploy_to, "/var/www/example.com"
+set :scm, :git
+set :branch, "master"
+set :user, "bill"
+set :group, "deployers"
+set :use_sudo, false
+set :rails_env, "production"
+set :deploy_via, :copy
+set :ssh_options, { :forward_agent => true, :port => 4321 }
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "example.com", :app, :web, :db, :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+
+  desc "Symlink shared config files"
+  task :symlink_config_files do
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+  end
+
+  # NOTE: I don't use this anymore, but this is how I used to do it.
+  desc "Precompile assets after deploy"
+  task :precompile_assets do
+    run <<-CMD
+      cd #{ current_path } &&
+      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+    CMD
+  end
+
+  desc "Restart applicaiton"
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  end
+end
+
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
+
+=end
