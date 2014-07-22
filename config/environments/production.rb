2406c0260d97c93b6422bf67ce1b27a1045d3072
josenova <novajose@gmail.com>
2014-07-22 14:18:49 -0400

    added blacks levels, production set to www.lafunda.com.do

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Lafunda::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: '190.4.89.88' }
+  config.action_mailer.default_url_options = { host: 'www.lafunda.com.do' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
