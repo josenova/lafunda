d8c7126052446f1994701c099906c53b0eddc5ae
josenova <novajose@gmail.com>
2014-08-05 16:35:26 -0400

    delayed job didnt work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :timeoutable, :rememberable, :validatable and :omniauthable, :registerable, :database_authenticatable, :recoverable
   devise :confirmable, :trackable, :lockable
+  #handle_asynchronously :send_confirmation_instructions, :queue => 'devise'
 
   has_many :inquiries, dependent: :destroy
 
