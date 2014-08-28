4a3cd3451c973e324cd79e75ea8aef1611d02283
josenova <novajose@gmail.com>
2014-08-28 09:40:46 -0400

    minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -151,11 +151,11 @@ if($(window).width() <= 568) {
 /******************************* PAYMENT PROVIDERS ALERTS **************************************/
 
 		$( "#providers #bank_transfer" ).click(function() {
-		  alert( "Banco BHD\nCuenta: 15216080019\nNombre: Kingsley Edgecombe\n\nColoca en la nota de depósito tu nombre e email registrado con nosotros." );
+		  alert( "Banco BHD\nCuenta: 15216080019\nNombre: Kingsley Edgecombe\n\nDepósitos\nColoca en la nota de depósito tu nombre e email registrado con nosotros.\n\nRetiros\nSolícita tu retiro a hfernandez@kinbar.com.do. Solo se aceptarán solicitudes y se enviarán pagos a cuentas de email que estén registradas con nosotros." );
 		});
 		
 		$( "#providers #paypal" ).click(function() {
-		  alert( "Paypal\nCuenta: hfernandez@kinbar.com.do\nNombre: Kinbar International\n\nDepósitos\nColoca en la nota de depósito tu nombre (e email registrado con nosotros, en caso de no usarlo para realizar el depósito).\n\nRetiros\nSolícita tu retiro a hfernandez@kinbar.com.do. Solo se aceptarán solicitudes y se enviarán pagos a cuentas de email que estén registradas con nosotros. " );
+		  alert( "Paypal\nCuenta: hfernandez@kinbar.com.do\nNombre: La Funda\n\nDepósitos\nColoca en la nota de depósito tu nombre (e email registrado con nosotros, en caso de no usarlo para realizar el depósito).\n\nRetiros\nSolícita tu retiro a hfernandez@kinbar.com.do. Solo se aceptarán solicitudes y se enviarán pagos a cuentas de email que estén registradas con nosotros. " );
 		});
 
  
