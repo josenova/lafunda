35c30242c527d2c4670a70b8c970d2a49fd86a88
josenova <novajose@gmail.com>
2014-09-09 16:08:55 -0400

    ticker today and yesterday winnings, lists now show item

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -14,21 +14,30 @@ $(document).ready(function() {
 	  accept: 'application/json',  
 	  dataType: 'json',
 	  success: function(data) {
-		  
-		console.log(data);
 		
 		var recent_date = new Date(data[0].ClosesOn);
+		var date_before = new Date();
+		date_before.setDate(recent_date.getDate() - 1);
+		
 		var slim_recent_date = recent_date.toString().slice(4,-50);
-
+		var slim_date_before = date_before.toString().slice(4,-50);
 		
-		$('.lotto_info #today').append(slim_recent_date);		
+		$('.lotto_info').append('<li class="date">' + slim_recent_date + '</li>');		
 		for (var i = 0; i < data.length; i++) { 
-		
 			var close_date = new Date(data[i].ClosesOn);
 			if (close_date.setHours(0, 0, 0, 0) == recent_date.setHours(0, 0, 0, 0)) {
     			$('.lotto_info').append('<li class="lotto_house"><span class="lotto_name">' + data[i].HouseName + '</span><span class="lotto_numbers">' + data[i].Drawings[0].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[1].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[2].PostedNumbers.replace(/\D/g,'') + '</span></li>');			
 			}
 		}
+			
+		$('.lotto_info').append('<li class="date">' + slim_date_before + '</li>');
+		for (var i = 0; i < data.length; i++) { 
+			var close_date = new Date(data[i].ClosesOn);
+			if (close_date.setHours(0, 0, 0, 0) == date_before.setHours(0, 0, 0, 0)) {
+    			$('.lotto_info').append('<li class="lotto_house"><span class="lotto_name">' + data[i].HouseName + '</span><span class="lotto_numbers">' + data[i].Drawings[0].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[1].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[2].PostedNumbers.replace(/\D/g,'') + '</span></li>');			
+			}
+		}
+		
 	  }
 	});
 
