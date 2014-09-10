481d776c8be4cb79a430d9e24059686920453522
josenova <novajose@gmail.com>
2014-09-10 17:05:54 -0400

    new banners, ticker now jquery

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	
 	$.ajax({
 	  type:'GET',
-	  url: 'https://lottery.lafunda.com.do/Lottery/WinningNumbers?key=664cf843-8904-4212-9503-d4733651f519&gobackdays=1&grouped=true',
+	  url: 'https://lottery.lafunda.com.do/Lottery/WinningNumbers?key=664cf843-8904-4212-9503-d4733651f519&gobackdays=2&grouped=true',
 	  accept: 'application/json',  
 	  dataType: 'json',
 	  success: function(data) {
@@ -22,24 +22,49 @@ $(document).ready(function() {
 		var slim_recent_date = recent_date.toString().slice(4,-50);
 		var slim_date_before = date_before.toString().slice(4,-50);
 		
-		$('.lotto_info').append('<li class="date">' + slim_recent_date + '</li>');		
+		var $lotto_info = $('.lotto_info');
+		
+		$lotto_info.append('<li class="date">' + slim_recent_date + '</li>');		
 		for (var i = 0; i < data.length; i++) { 
 			var close_date = new Date(data[i].ClosesOn);
 			if (close_date.setHours(0, 0, 0, 0) == recent_date.setHours(0, 0, 0, 0)) {
-    			$('.lotto_info').append('<li class="lotto_house"><span class="lotto_name">' + data[i].HouseName + '</span><span class="lotto_numbers">' + data[i].Drawings[0].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[1].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[2].PostedNumbers.replace(/\D/g,'') + '</span></li>');			
+    			$lotto_info.append('<li class="lotto_house"><span class="lotto_name">' + data[i].HouseName + '</span><span class="lotto_numbers"></span></li>');
+				for (var j = 0; j < data[i].Drawings.length; j++) {
+					var $lotto_numbers = $('.lotto_numbers').last();
+					$lotto_numbers.append(data[i].Drawings[j].PostedNumbers.replace(/\D/g,''));	
+					if (j != data[i].Drawings.length - 1) {$lotto_numbers.append(' / ')}		
+				} 
 			}
 		}
 			
-		$('.lotto_info').append('<li class="date">' + slim_date_before + '</li>');
+		$lotto_info.append('<li class="date">' + slim_date_before + '</li>');
 		for (var i = 0; i < data.length; i++) { 
 			var close_date = new Date(data[i].ClosesOn);
 			if (close_date.setHours(0, 0, 0, 0) == date_before.setHours(0, 0, 0, 0)) {
-    			$('.lotto_info').append('<li class="lotto_house"><span class="lotto_name">' + data[i].HouseName + '</span><span class="lotto_numbers">' + data[i].Drawings[0].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[1].PostedNumbers.replace(/\D/g,'') + ' / ' + data[i].Drawings[2].PostedNumbers.replace(/\D/g,'') + '</span></li>');			
+    			$lotto_info.append('<li class="lotto_house"><span class="lotto_name">' + data[i].HouseName + '</span><span class="lotto_numbers"></span></li>');
+				for (var j = 0; j < data[i].Drawings.length; j++) {
+					var $lotto_numbers = $('.lotto_numbers').last();
+					$lotto_numbers.append(data[i].Drawings[j].PostedNumbers.replace(/\D/g,''));	
+					if (j != data[i].Drawings.length - 1) {$lotto_numbers.append(' / ')}		
+				} 
 			}
 		}
 		
-	  }
-	});
+		
+		
+		function slideLeft() {
+        $lotto_info.animate({
+            marginLeft: "-6060px"
+        }, 60000, "linear", function() {
+            $lotto_info.css("margin-left", 0);
+            slideLeft();
+        });
+    }
+
+    slideLeft();
+		
+	  } // End Success function
+	}); //End Ajax
 
 });// END DOCUMENT READY
 
