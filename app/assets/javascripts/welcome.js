9d4cc4d36545136015696dda873319df9f5d266b
josenova <novajose@gmail.com>
2014-09-12 09:41:32 -0400

    ...

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,15 +1,3 @@
-if ('XDomainRequest' in window && window.XDomainRequest !== null) {
- 
-  // override default jQuery transport
-  jQuery.ajaxSettings.xhr = function() {
-      try { return new XDomainRequest(); }
-      catch(e) { }
-  };
- 
-  // also, override the support check
-  jQuery.support.cors = true;
- 
-}
 
 $(document).ready(function() {
 	
