39fc21c96ba1e8bf320659d51e41de086de9ba14
josenova <novajose@gmail.com>
2014-09-25 10:49:49 -0400

    resizer for race test

--- app/assets/javascripts/race_games.js
+++ app/assets/javascripts/race_games.js
@@ -10,8 +10,15 @@ if($(window).width() <= 568) {
 		  $('#race_frame').attr("src","https://wagering.lafunda.com.do/BOSSWagering/Racebook/MobileBetTaker?siteid=lafunda" + urlarray[1]);
 		  //$('#race_frame').attr("src","http://www.colchian.eu/BOSS_DEMO/BOSSWagering/Racebook/MobileBetTaker/?SiteID=LaFunda&stoken=" + urlarray[1]);
 
+} else {
+
+
+
+  $('#race_frame').iFrameResize({
+	  enablePublicMethods: true                  
+  });
+  
 }
- 
 
 });// END DOCUMENT READY
 
