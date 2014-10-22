455e332a27ea22e588da01191f58d9b56bb5c002
josenova <novajose@gmail.com>
2014-10-22 10:48:30 -0300

    ...

--- app/assets/javascripts/casino_games.js.js
+++ app/assets/javascripts/casino_games.js.js
@@ -8,7 +8,7 @@ if($(window).width() <= 768) {
 		  var url = $("#casino_frame").attr('src');
 		  var urlarray = url.split('=LaFunda');
 		  $('#casino_frame').attr("src","https://wagering.lafunda.com.do/BOSSWagering/Casino/InternetGaming?siteID=mobile&ssc=1014" + urlarray[1]);
-		  //$('#casino_frame').attr("src","http://www.colchian.eu/BOSS_DEMO/BOSSWagering/Casino/OnlineGaming/?SiteId=LaFunda&stoken=" + urlarray[1]);
+		  //$('#casino_frame').attr("src","https://www.colchian.eu/BOSS_DEMO/BOSSWagering/Casino/OnlineGaming?siteID=mobile&ssc=1014" + urlarray[1]);
 
 } else {
 
